3bdb0c187be630523aca020c86d1b32dc1b8e581
AndewKolovrat <andrewkolovrat@yandex.ru>
2020-10-20 16:41:28 +0300

    add fixes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-console.log('click');
-var video = document.getElementByById('presentation_video'),
+var video = document.getElementById('presentation_video'),
     playBtn = document.getElementById('presentation_button'),
-    controls = document.getElementById('presentation'),
     time = document.getElementById('presentation_Time');
 
 // запускам или останавливаем воспроизведение
 playBtn.addEventListener('click', function () {
-    console.log('click');
     if (video.paused) {
         video.play();
-        controls.classList.add('hidden');
+        playBtn.classList.add('visually-hidden');
     } else {
         video.pause();
-        controls.classList.remove('hidden');
+        playBtn.classList.remove('visually-hidden');
     }
 }, false);
 
 video.addEventListener('ended', function () {
     video.currentTime = 0;
-    controls.classList.remove('hidden');
+    playBtn.classList.remove('visually-hidden');
 }, false);
 
 video.addEventListener('click', function () {
@@ -26,5 +23,35 @@ video.addEventListener('click', function () {
 }, false);
 
 video.addEventListener('timeupdate', function () {
-    time.innerHTML = video.currentTime;
+    time.innerHTML = secondsToTime(video.currentTime);
 }, false);
+
+// рассчет отображаемого времени
+function secondsToTime(time) {
+
+    var h = Math.floor(time / (60 * 60)),
+        dm = time % (60 * 60),
+        m = Math.floor(dm / 60),
+        ds = dm % 60,
+        s = Math.ceil(ds);
+    if (s === 60) {
+        s = 0;
+        m = m + 1;
+    }
+    if (s < 10) {
+        s = '0' + s;
+    }
+    if (m === 60) {
+        m = 0;
+        h = h + 1;
+    }
+    if (m < 10) {
+        m = '0' + m;
+    }
+    if (h === 0) {
+        fulltime = m + ':' + s;
+    } else {
+        fulltime = h + ':' + m + ':' + s;
+    }
+    return fulltime;
+}
